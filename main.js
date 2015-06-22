18f7cc14bcfbd209b8afc82e9d88216756ad960d
juan2ramos <juan2ramos@gmail.com>
2015-06-21 22:27:22 -0500

    beta

---
+++ main.js
@@ -0,0 +1,74 @@
+
+function Persona(nombre, marcadores)
+{
+    this.nombre = nombre;
+    this.marcadores = marcadores;
+    this.puntaje ;
+    this.getPuntaje = function()
+    {
+        var puntajeParcial = 0;
+        for(i in this.marcadores){
+            
+            if( marcadoresGlobal[i][0] == marcadores[i][0] &&
+                marcadoresGlobal[i][1] == marcadores[i][1]){
+                puntajeParcial = puntajeParcial + 5;
+            
+                continue;
+            }
+
+            if(marcadoresGlobal[i][0] == marcadoresGlobal[i][1] &&
+                marcadores[i][0] && marcadores[i][1] ){
+                puntajeParcial = puntajeParcial + 3;
+                continue;
+            }
+
+            if( (marcadoresGlobal[i][0] > marcadoresGlobal[i][1] && 
+                marcadores[i][0] > marcadores[i][1]) ||
+
+                (marcadoresGlobal[i][0] < marcadoresGlobal[i][1] && 
+                marcadores[i][0] < marcadores[i][1])){
+                puntajeParcial = puntajeParcial + 2;
+                continue;
+
+            }
+
+            mgt = marcadoresGlobal[i][0] + marcadoresGlobal[i][1];
+            mt = marcadores[i][0] + marcadores[i][1];
+
+            if(mgt == mt){
+                puntajeParcial = puntajeParcial + 1;
+                continue;
+            }
+
+        }
+        this.puntaje = puntajeParcial;
+        return this.puntaje;
+    }
+}
+
+function inicio ()
+{
+    //nombre = document.getElementById('id')
+    //nombre.innerText = isa.nombre;
+    var participantes = {
+        "isa" : new Persona("Isa", isaPuntajes),
+        "german" :new Persona("german", germanPuntajes),
+        "andres" :new Persona("andres", andresPuntajes),
+        "leonardo" :new Persona("leonardo", leonardoPuntajes),
+        "juanZuluaga" :new Persona("juanZuluaga", juanZuluagaPuntajes),
+        "juanRamos" :new Persona("juanRamos", juanRamosPuntajes),
+        "diego" :new Persona("diego", diegoPuntajes),
+        "paola" :new Persona("paola", paolaPuntajes),
+        "fabian" :new Persona("fabian", fabianPuntajes),
+        "lenin" :new Persona("lenin", leninPuntajes)
+    
+    }
+    for(i in participantes){
+       console.log(i + " : " +participantes[i].getPuntaje()); 
+    }
+    
+    
+}
+
+
+
---
+++ resultados.js
@@ -0,0 +1,258 @@
+var marcadoresGlobal = {
+    "Chile - Ecuador "    : [ 2 , 0 ],
+    "México - Bolivia"    : [ 0 , 0 ],
+    "Ecuador - Bolivia"   : [ 2 , 3],
+    "Chile - México"  : [ 3 , 3 ],
+    "México - Ecuador"    : [ 1 , 2 ],
+    "Chile - Bolivia" : [ 5 , 0 ],
+    "Colombia – Venezuela"    : [ 0 , 1 ],
+    "Brasil - Perú"   : [ 2 , 1 ],
+    "Brasil – Colombia"  : [ 0 , 1 ],
+    "Perú - Venezuela" : [ 1 , 0 ],
+    "Colombia - Perú" : [ 0 , 0 ],
+    "Brasil – Venezuela"  : [ 2 , 1 ],
+    "Uruguay – Jamaica"   : [ 1 , 0 ],
+    "Argentina – Paraguay"    : [ 2 , 2 ],
+    "Paraguay - Jamaica"  : [ 1 , 0 ],
+    "Argentina – Uruguay" : [ 1 , 0 ],
+    "Uruguay - Paraguay"  : [ 1 , 1 ],
+    "Argentina - Jamaica" : [ 1 , 0 ],
+}
+var isaPuntajes ={
+    "Chile - Ecuador "  : [1,0],
+    "México - Bolivia"   : [3,1],
+    "Ecuador - Bolivia"   :[2,0],
+    "Chile - México"       : [1,1],
+    "México - Ecuador"   : [1,1],
+    "Chile - Bolivia"  : [2,0],
+    "Colombia – Venezuela"   :[2,0],
+    "Brasil - Perú"  : [2,0],
+    "Brasil – Colombia"  : [ 1 ,1 ],
+    "Perú - Venezuela"   : [2,0],
+    "Colombia - Perú"    : [2,0],
+    "Brasil – Venezuela" : [3,0],
+    "Uruguay – Jamaica" : [2,0],
+    "Argentina – Paraguay" : [2,2],
+    "Paraguay - Jamaica"   : [2,0],
+    "Argentina – Uruguay"  : [2,1],
+    "Uruguay - Paraguay"   : [2,1],
+    "Argentina - Jamaica" : [3,0],
+
+
+}
+var andresPuntajes = {
+    "Chile - Ecuador "  : [0,0],
+    "México - Bolivia"   : [3,0],
+    "Ecuador - Bolivia"   :[2,0],
+    "Chile - México"       : [1,2],
+    "México - Ecuador"   : [3,2],
+    "Chile - Bolivia"  : [3,1],
+    "Colombia – Venezuela"   :[3,0],
+    "Brasil - Perú"  : [2,0],
+    "Brasil – Colombia"  : [2,0],
+    "Perú - Venezuela"   : [1,1],
+    "Colombia - Perú"    : [3,0],
+    "Brasil – Venezuela" : [4,0],
+    "Uruguay – Jamaica" : [1,1],
+    "Argentina – Paraguay" : [3,1],
+    "Paraguay - Jamaica"   : [2,1],
+    "Argentina – Uruguay"  : [2,0],
+    "Uruguay - Paraguay"   : [1,1],
+    "Argentina - Jamaica" : [3,0],
+
+}
+
+var diegoPuntajes = {
+
+    "Chile - Ecuador "  : [0,0],
+    "México - Bolivia"   : [3,1],
+    "Ecuador - Bolivia"   :[2,0],
+    "Chile - México"       : [2,1],
+    "México - Ecuador"   : [0,1],
+    "Chile - Bolivia"  : [2,0],
+    "Colombia – Venezuela"   :[2,0],
+    "Brasil - Perú"  : [4,0],
+    "Brasil – Colombia"  : [4,0],
+    "Perú - Venezuela"   : [1,1],
+    "Colombia - Perú"    : [3,2],
+    "Brasil – Venezuela" : [3,0],
+    "Uruguay – Jamaica" : [2,1],
+    "Argentina – Paraguay" : [2,2],
+    "Paraguay - Jamaica"   : [0,1],
+    "Argentina – Uruguay"  : [2,2],
+    "Uruguay - Paraguay"   : [1,0],
+    "Argentina - Jamaica" : [4,0],
+
+}
+
+var fabianPuntajes = {
+
+    "Chile - Ecuador "  : [2,1],
+    "México - Bolivia"   : [1,0],
+    "Ecuador - Bolivia"   :[1,1],
+    "Chile - México"       : [1,2],
+    "México - Ecuador"   : [3,0],
+    "Chile - Bolivia"  : [3,2],
+    "Colombia – Venezuela"   :[3,0],
+    "Brasil - Perú"  : [4,1],
+    "Brasil – Colombia"  : [1,1],
+    "Perú - Venezuela"   : [2,0],
+    "Colombia - Perú"    : [2,0],
+    "Brasil – Venezuela" : [4,0],
+    "Uruguay – Jamaica" : [4,0],
+    "Argentina – Paraguay" : [3,1],
+    "Paraguay - Jamaica"   : [2,1],
+    "Argentina – Uruguay"  : [2,1],
+    "Uruguay - Paraguay"   : [2,2],
+    "Argentina - Jamaica" : [4,0],
+  
+
+}
+
+var germanPuntajes = {
+
+    "Chile - Ecuador "  : [0,1],
+    "México - Bolivia"   : [1,0],
+    "Ecuador - Bolivia"  :[2,0],
+    "Chile - México"       : [1,1],
+    "México - Ecuador"   : [2,1],
+    "Chile - Bolivia"  : [2,0],
+    "Colombia – Venezuela"   :[2,1],
+    "Brasil - Perú"  : [2,1],
+    "Brasil – Colombia"  : [2,2],
+    "Perú - Venezuela"   : [1,2],
+    "Colombia - Perú"    : [1,0],
+    "Brasil – Venezuela" : [3,0],
+    "Uruguay – Jamaica" : [3,0],
+    "Argentina – Paraguay" : [2,1],
+    "Paraguay - Jamaica"   : [2,3],
+    "Argentina – Uruguay"  : [2,2],
+    "Uruguay - Paraguay"   : [2,1],
+    "Argentina - Jamaica" : [3,0],
+   
+
+}
+
+var juanRamosPuntajes ={
+
+    "Chile - Ecuador "  : [2,0],
+    "México - Bolivia"   : [2,0],
+    "Ecuador - Bolivia"   :[1,0],
+    "Chile - México"       : [2,1],
+    "México - Ecuador"   : [1,0],
+    "Chile - Bolivia"  : [3,0],
+    "Colombia – Venezuela"   :[2,0],
+    "Brasil - Perú"  : [2,0],
+    "Brasil – Colombia"  : [1,1],
+    "Perú - Venezuela"   : [1,1],
+    "Colombia - Perú"    : [1,0],
+    "Brasil – Venezuela" : [2,0],
+    "Uruguay – Jamaica" : [2,0],
+    "Argentina – Paraguay" : [1,0],
+    "Paraguay - Jamaica"   : [2,0],
+    "Argentina – Uruguay"  : [2,1],
+    "Uruguay - Paraguay"   : [2,1],
+    "Argentina - Jamaica" : [3,0],
+
+   
+}
+
+
+var juanZuluagaPuntajes ={
+
+    "Chile - Ecuador "  : [3,0],
+    "México - Bolivia"   : [2,1],
+    "Ecuador - Bolivia"   :[1,1],
+    "Chile - México"       : [2,2],
+    "México - Ecuador"   : [4,1],
+    "Chile - Bolivia"  : [3,0],
+    "Colombia – Venezuela"   :[2,1],
+    "Brasil - Perú"  : [4,1],
+    "Brasil – Colombia"  : [3,2],
+    "Perú - Venezuela"   : [1,3],
+    "Colombia - Perú"    : [3,1],
+    "Brasil – Venezuela" : [2,0],
+    "Uruguay – Jamaica" : [2,1],
+    "Argentina – Paraguay" : [3,1],
+    "Paraguay - Jamaica"   : [1,1],
+    "Argentina – Uruguay"  : [2,1],
+    "Uruguay - Paraguay"   : [1,0],
+    "Argentina - Jamaica" : [2,0],
+
+    
+
+}
+
+var leninPuntajes = {
+
+    "Chile - Ecuador "  : [3,1],
+    "México - Bolivia"   : [2,0],
+    "Ecuador - Bolivia"   :[2,0],
+    "Chile - México"       : [2,2],
+    "México - Ecuador"   : [2,0],
+    "Chile - Bolivia"  : [4,1],
+    "Colombia – Venezuela"   :[1,0],
+    "Brasil - Perú"  : [2,0],
+    "Brasil – Colombia"  : [2,2],
+    "Perú - Venezuela"   : [1,1],
+    "Colombia - Perú"    : [2,0],
+    "Brasil – Venezuela" : [3,1],
+    "Uruguay – Jamaica" : [3,0],
+    "Argentina – Paraguay" : [3,1],
+    "Paraguay - Jamaica"   : [3,1],
+    "Argentina – Uruguay"  : [2,0],
+    "Uruguay - Paraguay"   : [1,1],
+    "Argentina - Jamaica" : [4,0],
+
+    
+}
+
+var leonardoPuntajes ={
+
+    "Chile - Ecuador "  : [3,0],
+    "México - Bolivia"   : [4,0],
+    "Ecuador - Bolivia"   :[3,1],
+    "Chile - México"       : [2,1],
+    "México - Ecuador"   : [1,1],
+    "Chile - Bolivia"  : [5,0],
+    "Colombia – Venezuela"   :[2,0],
+    "Brasil - Perú"  : [3,0],
+    "Brasil – Colombia"  : [2,1],
+    "Perú - Venezuela"   : [1,2],
+    "Colombia - Perú"    : [3,1],
+    "Brasil – Venezuela" : [2,0],
+    "Uruguay – Jamaica" : [3,0],
+    "Argentina – Paraguay" : [2,0],
+    "Paraguay - Jamaica"   : [2,1],
+    "Argentina – Uruguay"  : [2,1],
+    "Uruguay - Paraguay"   : [3,1],
+    "Argentina - Jamaica" : [3,0],
+
+   
+
+}
+
+var paolaPuntajes ={
+   
+    "Chile - Ecuador "  : [2,0],
+    "México - Bolivia"   : [1,1],
+    "Ecuador - Bolivia"   :[2,1],
+    "Chile - México"       : [0,2],
+    "México - Ecuador"   : [2,2],
+    "Chile - Bolivia"  : [1,0],
+    "Colombia – Venezuela"   :[3,0],
+    "Brasil - Perú"  : [4,1],
+    "Brasil – Colombia"  : [2,4],
+    "Perú - Venezuela"   : [1,0],
+    "Colombia - Perú"    : [3,0],
+    "Brasil – Venezuela" : [5,1],
+    "Uruguay – Jamaica" : [1,3],
+    "Argentina – Paraguay" : [2,1],
+    "Paraguay - Jamaica"   : [0,2],
+    "Argentina – Uruguay"  : [4,3],
+    "Uruguay - Paraguay"   : [2,2],
+    "Argentina - Jamaica" : [3,2],
+
+
+}
+
